709b5dd99c8e931a185757110761315e4a7a4d84
João Pedro Faria <jpalmeida.faria@gmail.com>
2021-09-20 17:05:57 -0300

    commit parcial

--- demo-mvc/src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
@@ -1,25 +1,17 @@
 package com.mballem.curso.boot.web.controller;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.mballem.curso.boot.domain.Cargo;
 import com.mballem.curso.boot.domain.Funcionario;
 import com.mballem.curso.boot.domain.UF;
 import com.mballem.curso.boot.service.CargoService;
 import com.mballem.curso.boot.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/funcionarios")
