a0f696ce7de78c8d604ea3cbad439986cf26b908
João Pedro Faria <jpalmeida.faria@gmail.com>
2021-09-20 12:27:12 -0300

    corrigindo problema com conexão ao banco de dados pelo jdbc

--- demo-mvc/src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -1,33 +1,20 @@
 package com.mballem.curso.boot.web.controller;
 
-import com.mballem.curso.boot.domain.Departamento;
-import com.mballem.curso.boot.service.DepartamentoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
-	@Autowired
-	private DepartamentoService service;
-
 	@GetMapping("/cadastrar")
-	public String cadastrar(Departamento departamento) {
+	public String cadastrar() {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "/departamento/lista"; 
-	}
-
-	@PostMapping("/salvar")
-	public String salvar(Departamento departamento) {
-		service.salvar(departamento);
-		return "redirect:/departamentos/cadastrar";
 	}
 }
