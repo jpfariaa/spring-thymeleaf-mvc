69dc74ee61d7b66314fcc528bb8a4ec9b1858cea
João Pedro Faria <jpalmeida.faria@gmail.com>
2021-09-20 14:22:18 -0300

    excluindo um departamento

--- demo-mvc/src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -16,37 +16,45 @@ import com.mballem.curso.boot.service.DepartamentoService;
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
-	@Autowired
-	private DepartamentoService service;
+    @Autowired
+    private DepartamentoService service;
 
-	@GetMapping("/cadastrar")
-	public String cadastrar(Departamento departamento) {
-		return "/departamento/cadastro";
-	}
+    @GetMapping("/cadastrar")
+    public String cadastrar(Departamento departamento) {
+        return "/departamento/cadastro";
+    }
 
-	@GetMapping("/listar")
-	public String listar(ModelMap model) {
-		model.addAttribute("departamentos", service.buscarTodos());
-		return "/departamento/lista";
-	}
+    @GetMapping("/listar")
+    public String listar(ModelMap model) {
+        model.addAttribute("departamentos", service.buscarTodos());
+        return "/departamento/lista";
+    }
 
-	@PostMapping("/salvar")
-	public String salvar(Departamento departamento, RedirectAttributes attr) {
-		service.salvar(departamento);
-		attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
-		return "redirect:/departamentos/cadastrar";
-	}
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento, RedirectAttributes attr) {
+        service.salvar(departamento);
+        attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
+        return "redirect:/departamentos/cadastrar";
+    }
 
-	@GetMapping("/editar/{id}")
-	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
-		model.addAttribute("departamento", service.buscarPorId(id));
-		return "/departamento/cadastro";
-	}
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
 
-	@PostMapping("/editar")
-	public String editar(Departamento departamento) {
-		service.editar(departamento);
-		return "redirect:/departamentos/cadastrar";
-	}
+    @PostMapping("/editar")
+    public String editar(Departamento departamento) {
+        service.editar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, ModelMap model) {
+        if (!service.departamentoTemCargos(id)) {
+            service.excluir(id);
+        }
+        return listar(model);
+    }
 }
 
